5e65e16ae436f8d3a2a586cf10c1caf14fc8035f
Daniel Pérez Rubio <danielperezr88@gmail.com>
2018-05-24 09:45:15 +0200

    no-login branch added

--- APIConsumer.py
+++ APIConsumer.py
@@ -1,29 +1,29 @@
-from flask import Flask, abort, flash, redirect, render_template, session, url_for, request
+from flask import Flask, redirect, render_template, url_for, request#, abort, flash, session
 
-import werkzeug.exceptions as ex
+#import werkzeug.exceptions as ex
 
-from os import urandom, path, getpid
+from os import urandom, getpid#, path
 from binascii import hexlify
 import inspect
 import logging
 
 import requests as req
 
-from hashlib import sha512
+#from hashlib import sha512
 
 import json
 
-from google.protobuf import timestamp_pb2
-from gcloud import storage
-
-ID_BUCKET = 'ids'
-
-
-def lookup_bucket(cli, prefix):
-    for bucket in cli.list_buckets():
-        if bucket.name.startswith(prefix):
-            return bucket.name
-    logging.error("Id Bucket not found")
+#from google.protobuf import timestamp_pb2
+#from gcloud import storage
+#
+#ID_BUCKET = 'ids'
+#
+#
+#def lookup_bucket(cli, prefix):
+#    for bucket in cli.list_buckets():
+#        if bucket.name.startswith(prefix):
+#            return bucket.name
+#    logging.error("Id Bucket not found")
 
 
 def save_pid():
@@ -39,15 +39,15 @@ logfilename = inspect.getfile(inspect.currentframe()) + ".log"
 logging.basicConfig(filename=logfilename, level=logging.INFO, format='%(asctime)s %(message)s')
 logging.info("Started")
 
-filepath = path.join(path.dirname(path.realpath(__file__)), 'tweetfeedplus_ids.py')
-if not path.exists(filepath):
-    client = storage.Client()
-    cblob = client.get_bucket(lookup_bucket(client, ID_BUCKET)).get_blob('tweetfeedplus_ids.py')
-    fp = open(filepath, 'wb')
-    cblob.download_to_file(fp)
-    fp.close()
-
-from tweetfeedplus_ids import id_dict as ids
+#filepath = path.join(path.dirname(path.realpath(__file__)), 'tweetfeedplus_ids.py')
+#if not path.exists(filepath):
+#    client = storage.Client()
+#    cblob = client.get_bucket(lookup_bucket(client, ID_BUCKET)).get_blob('tweetfeedplus_ids.py')
+#    fp = open(filepath, 'wb')
+#    cblob.download_to_file(fp)
+#    fp.close()
+#
+#from tweetfeedplus_ids import id_dict as ids
 
 def generate_url(host, protocol='http', port=80, dir=''):
 
@@ -56,22 +56,22 @@ def generate_url(host, protocol='http', port=80, dir=''):
 
     return "%s://%s:%d/%s" % (protocol, host, port, dir)
 
-class NotAllowed(ex.HTTPException):
-    code = 403
-    description = 'This is not the page you are looking for.'
-
-
-class NotValidToken(ex.HTTPException):
-    code = 401
-    description = 'Invalid Twitter Token'
-
-abort.mapping[401] = NotValidToken
-abort.mapping[403] = NotAllowed
-
-
-def no_impostors_wanted(s):
-    if (not s['logged_in']) if 'logged_in' in s.keys() else True:
-        abort(403)
+#class NotAllowed(ex.HTTPException):
+#    code = 403
+#    description = 'This is not the page you are looking for.'
+#
+#
+#class NotValidToken(ex.HTTPException):
+#    code = 401
+#    description = 'Invalid Twitter Token'
+#
+#abort.mapping[401] = NotValidToken
+#abort.mapping[403] = NotAllowed
+#
+#
+#def no_impostors_wanted(s):
+#    if (not s['logged_in']) if 'logged_in' in s.keys() else True:
+#        abort(403)
 
 
 API_IP = req.get(generate_url('jsonip.com')).json()['ip']
@@ -94,44 +94,50 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/logout')
-def logout():
-    del session['username']
-    session['logged_in'] = False
-    return redirect(url_for('index'))
+#@app.route('/logout')
+#def logout():
+#    del session['username']
+#    session['logged_in'] = False
+#    return redirect(url_for('index'))
 
 
 @app.route('/api/infer', methods=['POST'])
 def api_infer():
-    no_impostors_wanted(session)
+    #no_impostors_wanted(session)
     image = request.form['image']
     model = request.form['model']
     result = req.post(generate_url('localhost', dir=['api', 'infer'], port=88), data=json.dumps(dict(image=image, model=model)))
     return json.dumps(result.json())
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        uname = request.form['username']
-        if uname in ids.keys():
-            pwd = request.form['password']
-            pwd = pwd.encode('latin1')
-            digest = sha512(pwd).hexdigest()
-            if ids[uname] == digest:
-                session['username'] = request.form['username']
-                session['logged_in'] = True
-                return redirect(url_for('index'))
-            flash('Password did not match that for the login provided', 'bad_login')
-            return render_template('login.html')
-        flash('Unknown username', 'bad_login')
-    return render_template('login.html')
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    if request.method == 'POST':
+#        uname = request.form['username']
+#        if uname in ids.keys():
+#            pwd = request.form['password']
+#            pwd = pwd.encode('latin1')
+#            digest = sha512(pwd).hexdigest()
+#            if ids[uname] == digest:
+#                session['username'] = request.form['username']
+#                session['logged_in'] = True
+#                return redirect(url_for('index'))
+#            flash('Password did not match that for the login provided', 'bad_login')
+#            return render_template('login.html')
+#        flash('Unknown username', 'bad_login')
+#    return render_template('login.html')
 
 
 @app.route('/test_api', methods=['GET'])
 def test_api():
-    no_impostors_wanted(session)
+    #no_impostors_wanted(session)
     return render_template('test_api.html', API_IP=API_IP)
+
+@app.after_request
+def apply_caching(response):
+    #response.headers["X-Frame-Options"] = "ALLOW-FROM http://eh-aibjuwgkwtg.preview.forestry.io/"
+    response.headers["Content-Security-Policy"] = "frame-ancestors http://www.human-forecast.com http://www.human-forecast.com/* http://www.human-forecast.es http://www.human-forecast.es/* https://exp-ai.com https://exp-ai.com/* 'self'"
+    return response
 
 if __name__ == '__main__':
     run()
